6d88b2386c8b54d4d6b4025038cc1a87863afc4f
robis0910 <56173779+robis0910@users.noreply.github.com>
2025-03-21 04:02:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/api/persons', (request, response) => {
     }
 
     const person = {
-      id: generateId()
+      id: generateId(),
       name: body.name.trim(),
       number: body.number.trim()
     }
